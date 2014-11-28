b4a593f7ad4b077bd20ebe73799d1706e9d65618
Niraj Pant <nirajpant7@gmail.com>
2014-11-28 17:34:36 -0600

    Add API key and forecast url

--- SwiftWeather/ViewController.swift
+++ SwiftWeather/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let API_KEY = "f16eee6b1d2b74580f38750e03ea836c";
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+            let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(API_KEY)/")
+            let forecastURL = NSURL(string: "37.8267,-122.423", relativeToURL:baseURL)
     }
 
     override func didReceiveMemoryWarning() {
