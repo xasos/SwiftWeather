6098d888babf1c5f6fefae34dee0d242d8439686
Niraj Pant <nirajpant7@gmail.com>
2014-11-28 17:20:06 -0600

    Initial Commit

---
+++ SwiftWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftWeather
+//
+//  Created by Niraj  on 11/28/14.
+//  Copyright (c) 2014 Niraj Pant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
