5adc568828ae2456f28c60b70918844ae6658351
Niraj Pant <nirajpant7@gmail.com>
2014-12-07 12:31:50 -0600

    fix launch screen

--- SwiftWeather/ViewController.swift
+++ SwiftWeather/ViewController.swift
@@ -58,7 +58,23 @@ class ViewController: UIViewController {
                     self.refreshActivityIndicator.hidden = true
                     self.refreshButton.hidden = false
                 })
-            }            
+            }  else {
+                let networkIssueController = UIAlertController(title: "Error", message: "Unable to load data. Connectivity error!", preferredStyle: .Alert)
+                
+                let okButton = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                networkIssueController.addAction(okButton)
+                
+                let cancelButton = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+                networkIssueController.addAction(cancelButton)
+                
+                self.presentViewController(networkIssueController, animated: true, completion: nil)
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.refreshActivityIndicator.stopAnimating()
+                    self.refreshActivityIndicator.hidden = true
+                    self.refreshButton.hidden = false
+                })
+            }
         })
         downloadTask.resume()
     }
