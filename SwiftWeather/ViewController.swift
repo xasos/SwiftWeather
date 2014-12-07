5fc40f02e125c3f00473630f9c0875dbd5f07f6a
Niraj Pant <nirajpant7@gmail.com>
2014-12-07 12:13:36 -0600

    add async data retrieval

--- SwiftWeather/ViewController.swift
+++ SwiftWeather/ViewController.swift
@@ -11,27 +11,33 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var iconView: UIImageView!
-    
     @IBOutlet weak var currentTimeLabel: UILabel!
-    
     @IBOutlet weak var temperatureLabel: UILabel!
-    
     @IBOutlet weak var humidityLabel: UILabel!
     @IBOutlet weak var precipitationLabel: UILabel!
     @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    @IBOutlet weak var refreshActivityIndicator: UIActivityIndicatorView!
     
     private let API_KEY = "f16eee6b1d2b74580f38750e03ea836c";
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-       
+        refreshActivityIndicator.hidden = true
+        getCurrentWeatherData()
+
+    }
+    
+    
+    func getCurrentWeatherData() -> Void {
         let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(API_KEY)/")
         let forecastURL = NSURL(string: "37.8267,-122.423", relativeToURL:baseURL)
         
         let sharedSession = NSURLSession.sharedSession()
         let downloadTask: NSURLSessionDownloadTask = sharedSession.downloadTaskWithURL(forecastURL!, completionHandler: { (location: NSURL!, response: NSURLResponse!, error: NSError!) -> Void in
-        
+            
             if(error == nil) {
                 let dataObject = NSData(contentsOfURL: location)
                 let weatherDictionary: NSDictionary = NSJSONSerialization.JSONObjectWithData(dataObject!, options: nil, error: nil) as NSDictionary
@@ -46,13 +52,24 @@ class ViewController: UIViewController {
                     self.humidityLabel.text = "\(currentWeather.humidity)"
                     self.precipitationLabel.text = "\(currentWeather.precipProbability)"
                     self.summaryLabel.text = "\(currentWeather.summary)"
+                    
+                    //Stop refresh animation
+                    self.refreshActivityIndicator.stopAnimating()
+                    self.refreshActivityIndicator.hidden = true
+                    self.refreshButton.hidden = false
                 })
-                
-            }
-            
+            }            
         })
         downloadTask.resume()
     }
+    
+    @IBAction func refresh() {
+        refreshButton.hidden = true
+        refreshActivityIndicator.hidden = false
+        refreshActivityIndicator.startAnimating()
+        getCurrentWeatherData()
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
